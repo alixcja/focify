52b9ac6ad6b74030017e0a6e4608736384b4a9b0
alixcja <alicja.pater.2004@gmail.com>
2025-12-16 22:02:07 +0100

    ✨ Create tables on startup

--- src/main/de.alixcja/Application.kt
+++ src/main/de.alixcja/Application.kt
@@ -2,12 +2,32 @@ package de.alixcja
 
 import io.ktor.server.application.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+
+    connectToPostgresSql()
+    createTables()
 }
 
-fun Application.module() {
-    configureSerialization()
-    configureDatabases()
-    configureRouting()
+private fun createTables() {
+    logger.info { "Creating all necessary tables" }
+    transaction {
+        SchemaUtils.create(Vehicle)
+        logger.info { "Created `vehicle` table " }
+        SchemaUtils.create(Mileage)
+        logger.info { "Created `mileage` table " }
+        SchemaUtils.create(Refuel)
+        logger.info { "Created `refuel` table " }
+    }
+    logger.info { "Created all necessary tables" }
+}
+
+private fun connectToPostgresSql() {
+    logger.info { "Connecting to PostgresSQL database" }
+    Database.connect(
+        "jdbc:postgresql://localhost:5432/focify",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "postgres"
+    )
+    logger.info { "Successfully connected to PostgresSQL database" }
 }
